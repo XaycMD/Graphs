c837f5031161ae6892bec1b24a13b8e08816a73f
HouseMD <vpetyushka@gmail.com>
2018-11-25 13:33:43 +0200

    Add slidable panels with headers

---
+++ Assets/Scripts/AdjacencyMatrixUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace edu.ua.pavlusyk.masters
+{
+  public class AdjacencyMatrixUI : MonoBehaviour
+  {
+    
+  }
+}
